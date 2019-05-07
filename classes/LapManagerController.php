252dc9c5b25b628603d369a2d76732e63943980a
Vitor Paiva <vitor.paiva@localhost.localdomain>
2019-05-07 15:52:40 -0300

    Correção no caminho do arquivo

--- classes/LapManagerController.php
+++ classes/LapManagerController.php
@@ -7,7 +7,7 @@ class LapManagerController {
     	try{    		
 	        $file_extension = strtolower(substr($_FILES['raceInfo']['name'],-4)); 
 			$new_name = strtotime("now").trim($_FILES['raceInfo']['name']).$file_extension;
-			$dir = 'uploads/';
+			$dir = '/uploads/';
 			if(move_uploaded_file($_FILES['raceInfo']['tmp_name'], $dir.$new_name)){
 				return $dir.$new_name;
 			}
