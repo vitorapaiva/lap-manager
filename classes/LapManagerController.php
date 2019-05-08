51b43a35ddccdab8fd74f34a2fb6edcfa676b3ed
Vitor Paiva <vitor.paiva@localhost.localdomain>
2019-05-07 21:15:30 -0300

    Consertando erro sintax

--- classes/LapManagerController.php
+++ classes/LapManagerController.php
@@ -18,12 +18,12 @@ class LapManagerController {
         $arrayLap=$this->turnFileIntoArray($file);
         unset($arrayLap[0]);
         var_dump($arrayLap);
-    	$polePosition=array_filter($arrayLap, array($this, 'calculatesPolePosition');
-        $lapQty=array_filter($arrayLap, array($this, 'calculatesLapQty');
-        $totalRaceTime=array_filter($arrayLap, array($this, 'calculatesTotalRaceTime');
-        $bestLapByPilot=array_filter($arrayLap, array($this, 'calculateBestLapByPilot');
-        $bestLap=array_filter($arrayLap, array($this, 'calculateBestLap');
-        $averageSpeed=array_filter($arrayLap, array($this, 'calculateAverageSpeed');
-        $averageSpeed=array_filter($arrayLap, array($this, 'calculateAverageSpeed');
+    	$polePosition=array_filter($arrayLap, array($this, 'calculatesPolePosition'));
+        $lapQty=array_filter($arrayLap, array($this, 'calculatesLapQty'));
+        $totalRaceTime=array_filter($arrayLap, array($this, 'calculatesTotalRaceTime'));
+        $bestLapByPilot=array_filter($arrayLap, array($this, 'calculateBestLapByPilot'));
+        $bestLap=array_filter($arrayLap, array($this, 'calculateBestLap'));
+        $averageSpeed=array_filter($arrayLap, array($this, 'calculateAverageSpeed'));
+        $averageSpeed=array_filter($arrayLap, array($this, 'calculateAverageSpeed'));
     }
 }
