c39c65e707e27ff89e658febc427a62267ee5464
Vitor Paiva <vitor.paiva@localhost.localdomain>
2019-05-07 21:12:39 -0300

    Enviando modificações feitas

--- classes/LapManagerController.php
+++ classes/LapManagerController.php
@@ -7,7 +7,7 @@ class LapManagerController {
     		$raceInfo=file($fileName);
     		if($raceInfo){
     			foreach($raceInfo as $line_num => $lap){
-    				$arrayLap[$line_num]=explode('	',$lap);
+    				$arrayLap[$line_num]=array_filter(explode(' ',preg_replace('!\s+!', ' ', $lap)));
     			}
     		}
     	}
@@ -15,7 +15,15 @@ class LapManagerController {
     }
 
     public function processFile($file){
-    	$arrayLap=$this->turnFileIntoArray($file);
-    	var_dump($arrayLap);
+        $arrayLap=$this->turnFileIntoArray($file);
+        unset($arrayLap[0]);
+        var_dump($arrayLap);
+    	$polePosition=array_filter($arrayLap, array($this, 'calculatesPolePosition');
+        $lapQty=array_filter($arrayLap, array($this, 'calculatesLapQty');
+        $totalRaceTime=array_filter($arrayLap, array($this, 'calculatesTotalRaceTime');
+        $bestLapByPilot=array_filter($arrayLap, array($this, 'calculateBestLapByPilot');
+        $bestLap=array_filter($arrayLap, array($this, 'calculateBestLap');
+        $averageSpeed=array_filter($arrayLap, array($this, 'calculateAverageSpeed');
+        $averageSpeed=array_filter($arrayLap, array($this, 'calculateAverageSpeed');
     }
 }
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include ('classes/LapManagerController.php');
 if(isset($_FILES['raceInfo']))
 {
   $lapManager=new LapManagerController();
-  $raceResult=$lapManager->processFile($_FILES['raceInfo']);
+  $raceResult=$lapManager->processFile($_FILES['raceInfo']['tmp_name']);
 }
 
 require('views/index.view.php');
